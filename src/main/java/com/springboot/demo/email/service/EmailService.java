2753143a3aa7fe2bf2f16cbeb4be33eaf4759a2e
prasanth-git <prasanthmit@gmail.com>
2022-03-18 14:33:28 +0530

    'test'

--- src/main/java/com/springboot/demo/email/service/EmailService.java
+++ src/main/java/com/springboot/demo/email/service/EmailService.java
@@ -2,9 +2,14 @@ package com.springboot.demo.email.service;
 
 import java.io.File;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
 import javax.mail.MessagingException;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
@@ -22,14 +27,22 @@ public class EmailService {
 			throws MessagingException {
 
 		MimeMessage mimeMessage = mailSender.createMimeMessage();
+		MimeMultipart multipart = new MimeMultipart();
 
 		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
 		helper.addCc(new InternetAddress(to));
 		helper.setText(body);
 		helper.setSubject(subject);
 
-		FileSystemResource file = new FileSystemResource(new File(fileToAttach));
-		helper.addAttachment(file.getFilename(), file);
+		MimeBodyPart msg = new MimeBodyPart();
+		DataSource ds = new FileDataSource(fileToAttach);
+		msg.setDataHandler(new DataHandler(ds));
+		msg.setFileName(ds.getName());
+		multipart.addBodyPart(msg);
+		
+		mimeMessage.setContent(multipart);
+		//FileSystemResource file = new FileSystemResource(new File(fileToAttach));
+		//helper.addAttachment(file.getFilename(), file);
 
 		try {
 			mailSender.send(mimeMessage);
